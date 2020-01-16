d85bab130cebebf44ae5b2e7262edaa79ceee081
Andy Roush <aroush@StrategicCapital.com>
2020-01-16 07:49:29 -0800

    Updates

---
+++ precache-manifest.434880c6185d0f873286f30dc434f718.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28898efd21e59d15df7b51b3fad24aab",
+    "url": "/portfolio2020/index.html"
+  },
+  {
+    "revision": "a8debb2562b94d006e01",
+    "url": "/portfolio2020/static/css/main.0566b125.chunk.css"
+  },
+  {
+    "revision": "dc741462ef350b588eed",
+    "url": "/portfolio2020/static/js/2.725d387f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio2020/static/js/2.725d387f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8debb2562b94d006e01",
+    "url": "/portfolio2020/static/js/main.f9634944.chunk.js"
+  },
+  {
+    "revision": "fddc448e9034e60c954d",
+    "url": "/portfolio2020/static/js/runtime-main.ff1ec1be.js"
+  },
+  {
+    "revision": "55b2c45cb2586b7db5e7701824f3be3b",
+    "url": "/portfolio2020/static/media/codeBracket.55b2c45c.png"
+  },
+  {
+    "revision": "4d86d6b9993389752cad4b0baf9453da",
+    "url": "/portfolio2020/static/media/cv2.4d86d6b9.PNG"
+  },
+  {
+    "revision": "197a9a6abac36607c6b888785b017120",
+    "url": "/portfolio2020/static/media/gbimg.197a9a6a.PNG"
+  },
+  {
+    "revision": "073ea8b86447ed9310e7e73bd89894b3",
+    "url": "/portfolio2020/static/media/mern.073ea8b8.png"
+  },
+  {
+    "revision": "a74dfc8c79b7c0db0b523276aff7adff",
+    "url": "/portfolio2020/static/media/pfimg.a74dfc8c.PNG"
+  },
+  {
+    "revision": "311b5d2314412cd920b9a5d2787008cb",
+    "url": "/portfolio2020/static/media/rbimg.311b5d23.PNG"
+  },
+  {
+    "revision": "7bfec5dde4ad200b90278710410f70e1",
+    "url": "/portfolio2020/static/media/reaimg.7bfec5dd.PNG"
+  },
+  {
+    "revision": "920321c0dff7af91f50be37543c347c8",
+    "url": "/portfolio2020/static/media/scdimg.920321c0.PNG"
+  },
+  {
+    "revision": "8d3f4f996d7906d3703185fb4256e9f1",
+    "url": "/portfolio2020/static/media/snesButtons.8d3f4f99.png"
+  },
+  {
+    "revision": "71fbed80916b9a9a22f85e629bf09396",
+    "url": "/portfolio2020/static/media/triviaimg.71fbed80.PNG"
+  }
+]);
